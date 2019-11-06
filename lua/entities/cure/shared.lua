b1c713050fd88c52784f6e9b1f1ae6168d6a36c5
Nerdy196 <57379907+Nerdy196@users.noreply.github.com>
2019-11-06 09:13:50 -0500

    Delete shared.lua

--- lua/entities/cure/shared.lua
+++
@@ -1,9 +0,0 @@
-
------------------------------------------------------
-ENT.Type = "anim"
-ENT.Base = "base_gmodentity"
-ENT.PrintName = "Cure"
-ENT.Author = "___Nerd___"
-ENT.Category = "Nerds"
-ENT.Spawnable = true
-ENT.AdminSpawnable = true
